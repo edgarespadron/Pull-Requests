1a61f5d54881bee625231ca7d8da18083fa79e1b
Edgar <edgarespadron@yahoo.com>
2018-12-11 12:03:31 -0600

    transition

---
+++ 06week/rps.js
@@ -0,0 +1,16 @@
+alert("HELLO")
+
+
+var userChoice = promtp("Do you choose rock paper or scissors")
+
+var computerChoice= Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+} else if (computerChoice <= 0.67) {
+    computerChoice= "paper";
+} else {
+    ComputerChoice - "scissors";
+}
+console.log(Computer chooses: " +computerChoice0")
+
+    
